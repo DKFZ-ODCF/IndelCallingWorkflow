5fb588cec577ed6b5300a4fe863654c3ea65752e
Nagarajan Paramasivam <n.paramasivam@dkfz-heidelberg.de>
2017-09-18 09:51:32 +0200

    Bug fix in control vcf header retriever

--- resources/analysisTools/indelCallingWorkflow/checkSampleSwap_TiN.sh
+++ resources/analysisTools/indelCallingWorkflow/checkSampleSwap_TiN.sh
@@ -9,16 +9,16 @@ getRefGenomeAndChrPrefixFromHeader ${FILENAME_TUMOR_BAM} # Sets CHR_PREFIX and R
 
 ## Getting tumor and control header
 VCF_TUMOR_HEADER_COL=`samtools view -H ${FILENAME_TUMOR_BAM} | grep -P "^@RG" | perl -ne 'chomp; @s=split(/\t/, $_) ; map{if($_=~/SM:/){$_=~/SM:(.*)/; print "$1\n"}} @s;' | sort | uniq`
-VCF_CONTROL_HEADER_COL=`samtools view -H ${FILENAME_CONTROL_BAM} | grep -P "^@RG" | perl -ne 'chomp; @s=split(/\t/, $_) ; map{if($_=~/SM:/){$_=~/SM:(.*)/; print "$1\n"}} @s;' | sort | uniq`
+VCF_NORMAL_HEADER_COL=`samtools view -H ${FILENAME_CONTROL_BAM} | grep -P "^@RG" | perl -ne 'chomp; @s=split(/\t/, $_) ; map{if($_=~/SM:/){$_=~/SM:(.*)/; print "$1\n"}} @s;' | sort | uniq`
 
 if [[ -z $VCF_TUMOR_HEADER_COL ]] 
 then
   VCF_TUMOR_HEADER_COL=`basename ${FILENAME_TUMOR_BAM} | sed 's/.bam$//'`
 fi
 
-if [[ -z $VCF_CONTROL_HEADER_COL ]]
+if [[ -z $VCF_NORMAL_HEADER_COL ]]
 then
-  VCF_CONTROL_HEADER_COL=`basename ${FILENAME_CONTROL_BAM} | sed 's/.bam$//'`
+  VCF_NORMAL_HEADER_COL=`basename ${FILENAME_CONTROL_BAM} | sed 's/.bam$//'`
 fi
 
 ${PERL_BINARY} ${TOOL_CHECK_SAMPLE_SWAP_SCRIPT} \
